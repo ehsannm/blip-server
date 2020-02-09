d774c4af67b4925d76b1044c125257397e98c421
Ehsan N. Moosa <ehsan.code@nested.me>
2020-02-09 19:29:21 +0330

    fix

--- pkg/music/init.go
+++ pkg/music/init.go
@@ -44,22 +44,23 @@ func InitMongo(c *mongo.Client) {
 }
 
 func Init() {
+	httpClient.Transport = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSClientConfig:       &tls.Config{InsecureSkipVerify: true},
+	}
 	if config.GetString(config.HttpProxy) != "" {
 		if proxyURL, err := url.Parse(config.GetString(config.HttpProxy)); err == nil {
-			httpClient.Transport = &http.Transport{
-				Proxy: http.ProxyURL(proxyURL),
-				DialContext: (&net.Dialer{
-					Timeout:   30 * time.Second,
-					KeepAlive: 30 * time.Second,
-					DualStack: true,
-				}).DialContext,
-				ForceAttemptHTTP2:     true,
-				MaxIdleConns:          100,
-				IdleConnTimeout:       90 * time.Second,
-				TLSHandshakeTimeout:   10 * time.Second,
-				ExpectContinueTimeout: 1 * time.Second,
-				TLSClientConfig:       &tls.Config{InsecureSkipVerify: true},
-			}
+			httpClient.Transport.(*http.Transport).Proxy = http.ProxyURL(proxyURL)
 		} else {
 			log.Warn("Error On Set HTTP Proxy", zap.Error(err))
 		}
