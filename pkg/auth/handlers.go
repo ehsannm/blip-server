f85b41f37d1c65faf808706130cfcc6af1e923bd
Ehsan N. Moosa <ehsan.code@nested.me>
2020-02-29 17:11:08 +0330

    accept multiple response from VAS

--- pkg/auth/handlers.go
+++ pkg/auth/handlers.go
@@ -359,7 +359,9 @@ func LoginHandler(ctx iris.Context) {
 			msg.WriteError(ctx, http.StatusInternalServerError, msg.Item(err.Error()))
 			return
 		}
-		if vasCode != saba.SuccessfulCode {
+		switch vasCode {
+		case saba.SuccessfulCode, saba.SubscriptionAlreadyExists:
+		default:
 			msg.WriteError(ctx, http.StatusInternalServerError, msg.Item(saba.Codes[vasCode]))
 			return
 		}
