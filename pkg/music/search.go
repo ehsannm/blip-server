1499781c9ba62f5687b6a0ceac5968888728c881
Ehsan N. Moosa <ehsan.code@nested.me>
2020-07-24 17:20:13 +0430

    take it easy on search terms

--- pkg/music/search.go
+++ pkg/music/search.go
@@ -60,7 +60,7 @@ func SearchLocalIndex(keyword string, result int) ([]indexedSong, error) {
 	terms := strings.Split(strings.ToLower(keyword), "+")
 	for _, t := range terms {
 		t := strings.Trim(t, "()")
-		qs = append(qs, bleve.NewTermQuery(t))
+		qs = append(qs, bleve.NewTermQuery(t), bleve.NewPrefixQuery(t), bleve.NewFuzzyQuery(t))
 		if len(strings.Fields(t)) > 1 {
 			qs = append(qs, bleve.NewMatchQuery(t))
 		}
